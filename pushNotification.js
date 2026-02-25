94be35f5c0fb85d6f795ff028fa5f5471eb34d3c
Bryce King <bryceeking5@gmail.com>
2026-02-24 19:36:29 -0500

    comment removal

--- pushNotification.js
+++ pushNotification.js
@@ -27,4 +27,3 @@ export async function sendPush(deviceTokens, notiTitle, notiBody) {
       const result = await apnProvider.send(notification, userToken.token);
     }
   }
-  //sendPush("6218e5feae8be36d4c30948da79cdab0b0dfddc3339437961ca8625e05b92c78", "title", "body")
