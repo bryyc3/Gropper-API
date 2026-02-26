c9520c2b36cb9ea5ba1b572f19c59139b6b23c16
Bryce King <bryceeking5@gmail.com>
2026-02-26 16:00:15 -0500

    Dev test variables when logging in

--- routing/otp.js
+++ routing/otp.js
@@ -1,34 +1,47 @@
 import { Router } from 'express';
 import * as otpService from '../authentication/sms.js';
 import * as jwtService from '../authentication/jwtUtils.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const router = Router();
 
 router.post('/generate-otp', async (req, res) =>{
+  console.log("generating")
     const userPhoneNumber = req.body.phoneNumber;
-  
-    const otpSent = await otpService.sendOtp(userPhoneNumber);
-    
-    if(otpSent === "pending"){
+
+    if(userPhoneNumber == process.env.DEVTEST_PHONE){
       res.status(200).send(true)
+    } else {
+      const otpSent = await otpService.sendOtp(userPhoneNumber);
+      if(otpSent === "pending"){
+        res.status(200).send(true)
+      }
     }
 })//send user otp for authorization 
   
 router.post('/verify-otp', async (req, res) =>{
     const userPhoneNumber = req.body.phoneNumber;
     const enteredOtp = JSON.parse(req.body.userCode);
-     
-    const otpVerified =  await otpService.verifyOtp(userPhoneNumber, enteredOtp)
-  
-    if(otpVerified === "approved"){
+    
+    if(userPhoneNumber == process.env.DEVTEST_PHONE && enteredOtp == process.env.DEVTEST_PASSWORD){
       const refresh = jwtService.generateRefreshToken(userPhoneNumber);
       const access = jwtService.generateAccessToken(userPhoneNumber);
       const tokens = {refreshToken: refresh, accessToken: access};
       res.json(tokens);
-    }
-    if(otpVerified === "pending"){
-      console.log("Incorrect password");
-      res.status(400)
+    } else{
+      const otpVerified =  await otpService.verifyOtp(userPhoneNumber, enteredOtp)
+  
+      if(otpVerified === "approved"){
+        const refresh = jwtService.generateRefreshToken(userPhoneNumber);
+        const access = jwtService.generateAccessToken(userPhoneNumber);
+        const tokens = {refreshToken: refresh, accessToken: access};
+        res.json(tokens);
+      }
+      if(otpVerified === "pending"){
+        console.log("Incorrect password");
+        res.status(400)
+      }
     }
 })//verify otp sent to user via sms
 
